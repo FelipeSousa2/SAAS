ab0f0eb3f68d337b3064fd1f7841fb4c62e56a32
FelipeSousa@ <103294274+FelipeSousa2@users.noreply.github.com>
2023-09-05 22:22:55 -0300

    atualizado

--- restaurante/src/components/Offer.tsx
+++ restaurante/src/components/Offer.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import CountDown from "./Countdown";
+import CountDown from "./CountDown";
 
 export default function Offer() {
   return (
-    <div className="bg-black h-screen flex flex-col md:flex-row">
+    <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]">
       {/* Text container */}
       <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
         <h1 className="text-white text-5xl font-bold xl:text-6xl">
@@ -15,10 +15,12 @@ export default function Offer() {
           em várias frentes.
         </p>
         <CountDown />
-        <button className="bg-red-500 text-white rounded-md py-3 px-6">Peça agora</button>
+        <button className="bg-red-500 text-white rounded-md py-3 px-6">
+          Peça agora
+        </button>
       </div>
       {/* Image container */}
-      <div className="flex-1 w-full relative">
+      <div className="flex-1 w-full relative md:h-full">
         <Image src="/offerProduct.png" alt="" fill className="object-contain" />
       </div>
     </div>
---
+++ restaurante/src/components/Price.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { type } from "os";
+import { useEffect, useState } from "react";
+
+type Props = {
+  price: number;
+  id: number;
+  options?: { title: string; additionalPrice: number }[];
+};
+
+export default function Price({ price, id, options }: Props) {
+  const [total, setTotal] = useState(price);
+  const [quantity, setQuantity] = useState(1);
+  const [selected, setSelected] = useState(0);
+
+  useEffect(() => {
+    setTotal(
+      quantity * (options ? price + options[selected].additionalPrice : price)
+    );
+  }, [quantity, selected, options, price]);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="text-2xl font-bold">R${total.toFixed(2)}</h2>
+      {/* Options container */}
+      <div className="flex gap-4">
+        {options?.map((options, index) => (
+          <button
+            key={options.title}
+            className="min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md"
+            style={{
+              background: selected === index ? "rgb(248 113 113)" : "white",
+              color: selected === index ? "white" : "red",
+            }}
+            onClick={() => setSelected(index)}
+          >
+            {options.title}
+          </button>
+        ))}
+      </div>
+      {/* Quantity and add button container */}
+      <div className="flex justify-between items-center">
+        {/* Quantity */}
+        <div className="flex justify-between w-full p-3 ring-1 ring-red-500">
+          <span>Quantidade</span>
+          <div className="flex gap-4 items-center">
+            <button
+              onClick={() => setQuantity((prev) => (prev > 1 ? prev - 1 : 1))}
+            >
+              {"<"}
+            </button>
+            <span>{quantity}</span>
+            <button
+              onClick={() => setQuantity((prev) => (prev < 9 ? prev + 1 : 9))}
+            >
+              {">"}
+            </button>
+          </div>
+        </div>
+        {/* Cart button */}
+        <button className="uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-400">
+          Adicionar
+        </button>
+      </div>
+    </div>
+  );
+}
