8226519821be2da397e99c6fb8494b83530e7d7e
FelipeSousa@ <103294274+FelipeSousa2@users.noreply.github.com>
2023-09-08 23:51:33 -0300

    conexao com o banco de dados prisma

--- restaurante/src/app/menu/page.tsx
+++ restaurante/src/app/menu/page.tsx
@@ -1,8 +1,21 @@
-import { menu } from "@/data";
+import { MenuType } from "@/types/types";
 import Category from "./[category]/page";
 import Link from "next/link";
 
-export default function MenuPage() {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed!");
+  }
+
+  return res.json();
+};
+
+const MenuPage = async () => {
+  const menu: MenuType = await getData();
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-7rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
@@ -27,4 +40,6 @@ export default function MenuPage() {
       ))}
     </div>
   );
-}
+};
+
+export default MenuPage;
