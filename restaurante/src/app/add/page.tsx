69dd541b52426ddd638356eabb458b5d6b822e0a
FelipeSousa@ <103294274+FelipeSousa2@users.noreply.github.com>
2023-10-10 23:58:04 -0300

    atualizando

--- restaurante/src/app/add/page.tsx
+++ restaurante/src/app/add/page.tsx
@@ -1,46 +1,199 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
+import React, { useState } from "react";
 
+type Inputs = {
+  title: string;
+  desc: string;
+  price: number;
+  catSlug: string;
+};
+
+type Option = {
+  title: string;
+  additionalPrice: number;
+};
 
 const AddPage = () => {
-    const {} = useSession()
+  const { data: session, status } = useSession();
+  const [inputs, setInputs] = useState<Inputs>({
+    title: "",
+    desc: "",
+    price: 0,
+    catSlug: "",
+  });
+
+  const [option, setOption] = useState<Option>({
+    title: "",
+    additionalPrice: 0,
+  });
+
+  const [options, setOptions] = useState<Option[]>([]);
+  const [file, setFile] = useState<File>();
+
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (status === "unauthenticated" || !session?.user.isAdmin) {
+    router.push("/");
+  }
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const ChangeOption = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOption((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleChangeImg = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    const item = (target.files as FileList)[0];
+    setFile(item);
+  };
+
+  const upload = async () => {
+    const data = new FormData();
+    data.append("file", file!);
+    data.append("upload_preset", "restaurante");
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/dt112iqct/image", {
+      method: "POST",
+      headers: { "Content-Type": "multipart/form-data" },
+      body: data,
+    });
+
+    const resData = await res.json();
+    return resData.url;
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const url = await upload();
+      const res = await fetch("http://localhost:3000/api/products", {
+        method: "POST",
+        body: JSON.stringify({
+          img: url,
+          ...inputs,
+          options,
+        }),
+      });
+
+      const data = await res.json();
+
+      // router.push(`/product/${data.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div>
-      <form>
+      <form
+        className="shadow-lg flex flex-wrap gap-4 p-8"
+        onSubmit={handleSubmit}
+      >
         <h1>Adicionar Novo Produto</h1>
-        <div>
+        <div className="w-full flex flex-col gap-2">
+          <label>Imagem</label>
+          <input
+            className="ring-1 ring-red-200 p-2 rounded-sm"
+            type="file"
+            onChange={handleChangeImg}
+          />
+        </div>
+        <div className="w-full flex flex-col gap-2">
           <label>Título</label>
-          <input type="text" name="title" />
+          <input
+            onChange={handleChange}
+            className="ring-1 ring-red-200 p-2 rounded-sm"
+            type="text"
+            name="title"
+          />
         </div>
-        <div>
+        <div className="w-full flex flex-col gap-2">
           <label>Descrição</label>
-          <textarea  name="desc" />
+          <textarea
+            className="ring-1 ring-red-200 p-2 rounded-sm"
+            name="desc"
+            onChange={handleChange}
+          />
         </div>
-        <div>
+        <div className="w-full flex flex-col gap-2">
           <label>Preço</label>
-          <input type="numebr" name="price" />
+          <input
+            onChange={handleChange}
+            className="ring-1 ring-red-200 p-2 rounded-sm"
+            type="numebr"
+            name="price"
+          />
         </div>
-        <div>
+        <div className="w-full flex flex-col gap-2">
           <label>Categoria</label>
-          <input type="text" name="title" />
+          <input
+            onChange={handleChange}
+            className="ring-1 ring-red-200 p-2 rounded-sm"
+            type="text"
+            name="catSlug"
+          />
         </div>
-        <div>
+        <div className="w-full flex flex-col gap-2">
           <label>Opções</label>
           <div>
-            <input type="text" placeholder="Título" name="title" />
-            <input type="number" placeholder="Preço Adicional" name="additionalPrice" />
+            <input
+              onChange={ChangeOption}
+              className="ring-1 ring-red-200 p-2 rounded-sm"
+              type="text"
+              placeholder="Título"
+              name="title"
+            />
+            <input
+              onChange={ChangeOption}
+              className="ring-1 ring-red-200 p-2 rounded-sm"
+              type="number"
+              placeholder="Preço Adicional"
+              name="additionalPrice"
+            />
           </div>
-          <button>Adicionar opção</button>
+          <div
+            className="w-52 bg-red-500 text-white p-2"
+            onClick={() => setOptions((prev) => [...prev, option])}
+          >
+            Adicionar opção
+          </div>
         </div>
         <div>
-            <div>
-                <span>Pequena</span>
-                <span>R$ 2</span>
+          {options.map((item) => (
+            <div
+              className="ring-1 p-2 ring-red-500 rounded-md cursor-pointer"
+              key={item.title}
+              onClick={() =>
+                setOptions(options.filter((opt) => opt.title !== item.title))
+              }
+            >
+              <span>{item.title}</span>
+              <span>R$ {item.additionalPrice}</span>
             </div>
+          ))}
         </div>
+        <button type="submit" className="p-2 w-full bg-red-500 text-white">
+          Envair
+        </button>
       </form>
     </div>
   );
